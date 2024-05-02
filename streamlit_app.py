26d28a1c42c0b12c1b2c14e37d593aa8f322b083
AORNG <167181348+AORNG@users.noreply.github.com>
2024-05-02 08:56:53 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,8 @@
-import streamlit as st
+import streamlit as streamlit
 import random
+st.title("おみくじアプリ")
+if st.button("おみくじを引く"):
+    results=["大吉","中吉","小吉","吉","凶","大凶"]
+    result=random.choice(results)
+    st.wtrite(f"結果:{reuslt}")
 
-def main():
-    st.title('くじ引きアプリ')
-
-    user_name=st.text_input(名前を入力)
-
-    # くじのリストを定義
-    kuji_list = ['大吉', '中吉', '小吉', '凶', '大凶']
-
-    # くじを引くためのボタン
-    if st.button('くじを引く'):
-        # くじ引きの結果をランダムに選択
-        result = random.choice(kuji_list)
-        comments={
-            "大吉":"すごい"
-        }
-        comment=comments[result]
-        st.write(f'結果: {result}')
-
-if __name__ == '__main__':
-    main()
