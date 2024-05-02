a3b2a440f9eeefd4af2e93633ca778b350439030
AORNG <167181348+AORNG@users.noreply.github.com>
2024-05-02 09:00:02 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.title("おみくじアプリ")
 if st.button("おみくじを引く"):
     results=["大吉","中吉","小吉","吉","凶","大凶"]
     result=random.choice(results)
-    st.wtrite(f"結果:{result}")
+    st.write(f"結果:{result}")
 
