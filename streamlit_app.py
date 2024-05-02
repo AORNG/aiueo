fc0f1d34b0ae1308fa7c0fa20c6ca4114f730f0d
AORNG <167181348+AORNG@users.noreply.github.com>
2024-05-02 08:47:26 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import random
 def main():
     st.title('くじ引きアプリ')
 
+    user_name=st.text_input(名前を入力)
+
     # くじのリストを定義
     kuji_list = ['大吉', '中吉', '小吉', '凶', '大凶']
 
@@ -11,6 +13,10 @@ def main():
     if st.button('くじを引く'):
         # くじ引きの結果をランダムに選択
         result = random.choice(kuji_list)
+        comments={
+            "大吉":"すごい"
+        }
+        comment=comments[result]
         st.write(f'結果: {result}')
 
 if __name__ == '__main__':
