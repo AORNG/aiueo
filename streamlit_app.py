885553518bc3ee67118e89691076946428f265d9
AORNG <167181348+AORNG@users.noreply.github.com>
2024-05-02 09:03:10 +0000

    １

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import random
 st.title("おみくじアプリ")
+#ユーザー名の入力
+user_name=st.text_input("あなたの名前を入力してください。")
 if st.button("おみくじを引く"):
     results=["大吉","中吉","小吉","吉","凶","大凶"]
     result=random.choice(results)
