83e3816167669cb8da0423ebaca4f3a9162c6a80
AORNG <167181348+AORNG@users.noreply.github.com>
2024-05-08 08:02:41 +0000

    v

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,12 @@ st.write("あなたの体重は"+str(weight)+"kgです")
 a=weight-10
 
 st.write(str(a)+"kgを目指しましょう")
+
+st.text("身長を入力してください")
+
+long=st.number_input("身長を入力")
+
+st.write("あなたの身長は"+str(weight+"cmです"))
+b=long+10
+
+st.write(str(b)+"cmを目指しましょう)
