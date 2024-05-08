68e64b7a898ee9b99bcab0b618324de6593f8d8a
AORNG <167181348+AORNG@users.noreply.github.com>
2024-05-08 07:41:52 +0000

    h

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,5 @@
 import streamlit as st
 import random
-st.title("おみくじアプリ")
-#ユーザー名の入力
-user_name=st.text_input("あなたの名前を入力してください。")
-if st.button("おみくじを引く"):
-    results=["大吉","中吉","小吉","吉","凶","大凶"]
-    result=random.choice(results)
-
-#結果に応じたコメントやアドバイス
-comments={
-    "大吉":"わあー",
-    "中吉":"おおー",
-    "小吉":"まあまあ",
-    "吉":"そうか",
-    "凶":"ドンマイ",
-    "大凶":"はっはっは"
-}
-
-#結果とコメントの表示
-if user_name:
-    st.write(f"{user_name}さんの運命:{result}")
-else:
-    st.write(f"運命:{result}")
-
-st.write(comments[result])
+st.title("BMIアプリ")
+st.text("あなたの体重を入力してください。")
+st.
