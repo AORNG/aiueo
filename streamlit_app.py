026c0281984c63382bfb5d65f031f1240efcbc10
AORNG <167181348+AORNG@users.noreply.github.com>
2024-05-08 07:51:51 +0000

    い

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+
 st.title("BMI計算アプリ")
 st.text("体重を入力してください")
+
 weight=st.number_input("体重を入力")
+
+st.write("あなたの体重は"+str(weight)+"kgです")
+a=eight-10
+
+st.write("理想体重は"+str(a)+"kgです")
