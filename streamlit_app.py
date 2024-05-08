311da452474e7a81ecd89d4e6bab6da3d9dc25b6
AORNG <167181348+AORNG@users.noreply.github.com>
2024-05-08 08:06:53 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("BMI計算アプリ")
 st.text("体重を入力してください")
 
-weight=st.number_input("体重を入力")
+weight=st.number_input("体重を入力(kg)")
 
 st.write("あなたの体重は"+str(weight)+"kgです")
 a=weight-10
@@ -12,11 +12,11 @@ st.write(str(a)+"kgを目指しましょう")
 
 st.text("身長を入力してください")
 
-long=st.number_input("身長を入力")
+long=st.number_input("身長を入力(m)")
 
-st.write("あなたの身長は"+str(weight)+"cmです")
+st.write("あなたの身長は"+str(weight)+"mです")
 b=long+10
 
-st.write(str(b)+"cmを目指しましょう")
+st.write(str(b)+"mを目指しましょう")
 a=weight/long**2
 st.text(a)
