6d5930643971fd245669238150f0d4a5b1abb7fd
AORNG <167181348+AORNG@users.noreply.github.com>
2024-05-21 08:49:24 +0000

    ｓ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,5 @@
 import streamlit as st
 
-st.title("BMI計算アプリ")
-st.text("体重を入力してください")
+st.title("生物ガチャアプリ")
+st.text("あなたたちの生物に弱弱しい脳を仕方なく鍛えて差し上げましょう！")
 
-weight=st.number_input("体重を入力(kg)")
-
-st.write("あなたの体重は"+str(weight)+"kgです")
-a=weight-10
-
-st.write(str(a)+"kgを目指しましょう")
-
-st.text("身長を入力してください")
-
-long=st.number_input("身長を入力(m)")
-
-st.write("あなたの身長は"+str(weight)+"mです")
-b=long+0.1
-
-st.write(str(b)+"mを目指しましょう")
-if st.button("計算"):
-    c=weight/long**2
-    st.text(c)
