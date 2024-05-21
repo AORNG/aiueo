f7af1d7d2b9ac0b087d4df40bd1cabfc5b701eb0
AORNG <167181348+AORNG@users.noreply.github.com>
2024-05-21 09:08:36 +0000

    ｄ

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title("生物ガチャアプリ")
 st.text("このガチャを通して、生物について学ぼう！")
 
-if st.button("Enter"):
-    if st.button("o"):
-        st.text("rere")
+if st.button("ガチャを回しましょう。"):
+    st.button("o")
+        
