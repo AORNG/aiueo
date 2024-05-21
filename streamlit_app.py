44f541a417c53399ca209517386a09c07997c742
AORNG <167181348+AORNG@users.noreply.github.com>
2024-05-21 09:06:57 +0000

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,6 @@ import streamlit as st
 st.title("生物ガチャアプリ")
 st.text("このガチャを通して、生物について学ぼう！")
 
-if st.button("o"):
-    st.text("i")
+if st.button("Enter"):
+    if st.button("o"):
+        st.text("g")
