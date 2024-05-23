f4f130318dd9b1d396f6df4bf6a6369b833814af
AORNG <167181348+AORNG@users.noreply.github.com>
2024-05-23 08:41:06 +0000

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import random
 
-st.title("生物ガチャアプリ")
-st.text("このガチャを通して、生物について学ぼう！")
-
-if st.button("ガチャを回しましょう。"):
-    results = ["鰯","蛙","蛇","雀","猿"]
-    result = random.choice(results)
-    st.text(result + "、特徴言える？")
+st.title("歴史単語ガチャ")
+st.write ("歴史に強くなりましょう！")
+a = ["2020年から流行ったウイルスの名前は何か？"]
+b = ["1939年9月1日に起こったことは何か？"]
+c = ["2020年から流行ったウイルスの名前は何か？","1939年9月1日に起こったことは何か？"]
+c = random.choice(a,b)
+d = ["新型コロナウイルス"]
+e = ["第二次世界大戦"]
+k = ["新型コロナウイルス","第二次世界大戦"]
+g = random.choice(k)
+if st.button("勉強を開始する"):
+   st.write(f"問題:{c}")
+   if st.button("解答を見る"):
+      st.write(f"解答:{g}")
