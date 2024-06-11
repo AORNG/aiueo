095971bcf7a3869ade27a45d1c12683e1b2af550
AORNG <167181348+AORNG@users.noreply.github.com>
2024-06-11 09:10:13 +0000

    i

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import streamlit as st
 
 # Montserratフォントを使ったタイトルを表示
 st.markdown("<h1 style='text-align: center; font-family: Open Sans, sans-serif;'>生物単語ガチャ</h1>", unsafe_allow_html=True)
