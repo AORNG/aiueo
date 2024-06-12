6306737263bafffe42bb29ab576845f247a927b1
AORNG <167181348+AORNG@users.noreply.github.com>
2024-06-12 07:35:25 +0000

    w

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,9 +56,9 @@ if 'selected_word' in st.session_state:
             main()
             if 'selected_word' in st.session_state:
                 if st.button('解答の表示'):
-                    st.header(f"単語名: {st.session_state.selected_word['単語']}")
+                    st.write(f"単語名: {st.session_state.selected_word['単語']}")
             if 'selected_word' in st.session_state:
                 if st.button('レア度の表示'):
-                    st.subheader(f"レア度: {st.session_state.selected_word['レア度']}")
+                    st.write(f"レア度: {st.session_state.selected_word['レア度']}")
 
     
