51a2b3d052e95dda62fefc7168e406bd0355e817
AORNG <167181348+AORNG@users.noreply.github.com>
2024-06-13 09:06:47 +0000

    ｗ

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,8 +47,9 @@ if 'selected_word' in st.session_state:
     correct_answer = st.session_state.selected_word['単語']
     wrong_answers = words_df[words_df['レア度'] != st.session_state.selected_word['レア度']]['単語'].tolist()
 
-    # 正解を含む選択肢をランダムに選ぶ
+    # 正解を含む選択肢をランダムに選び、その並び順をランダムにする
     options = random.sample([correct_answer] + wrong_answers, 4)
+    random.shuffle(options)
 
     # 解答選択肢を表示
     user_answer = st.radio("解答を選択してください", options)
