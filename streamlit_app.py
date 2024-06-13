3412e8c3ce7a63bf2ad8df64e65f74c3463963aa
AORNG <167181348+AORNG@users.noreply.github.com>
2024-06-13 08:32:52 +0000

    ｐ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,58 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-def main():
-    st.title("解答確認アプリ")
+# Montserratフォントを使ったタイトルを表示
+st.markdown("<h1 style='text-align: center; font-family: Open Sans, sans-serif;'>生物単語ガチャ</h1>", unsafe_allow_html=True)
+css = """
+h1 {
+    color: #00CED1; /* タイトルの文字色を赤に変更 */
+}
+"""
 
-    # 問題を表示
-    question = "2 + 2 は？"
-    st.write("問題:", question)
+# CSSを適用する
+st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+st.write('生物用語をランダムに表示して、勉強をサポートします！')
+st.write('がんばってください！')
 
+# Load the data
+@st.cache
+def load_data():
+    return pd.read_excel("生物ガチャ.xlsx")
+
+words_df = load_data()
+
+# ガチャ機能
+if st.button('ガチャを引く！'):
+    rarity_probs = {
+        'N': 0.4,
+        'R': 0.3,
+        'SR': 0.2,
+        'SSR': 0.1
+    }
+    chosen_rarity = np.random.choice(list(rarity_probs.keys()), p=list(rarity_probs.values()))
+    subset_df = words_df[words_df['レア度'] == chosen_rarity]
+    selected_word = subset_df.sample().iloc[0]
+    
+    # セッションステートに選択された単語を保存
+    st.session_state.selected_word = selected_word
+    st.session_state.display_meaning = False
+
+if 'selected_word' in st.session_state:
+    st.title(f"Q: {st.session_state.selected_word['説明']}")
     # 解答欄を作成
     user_answer = st.text_input("解答を入力してください")
 
-    # 解答合わせボタン
+    # 答え合わせボタン
     if st.button("答え合わせ"):
-        correct_answer = 4  # 正解を定義
+        correct_answer = st.session_state.selected_word['単語']   # 正解を定義
 
         # 解答が正しいかどうかを確認し、結果を表示
         if user_answer.strip() == str(correct_answer):
-            st.success("正解です！正しい答えは" + str(correct_answer) + "です。")  # 成功メッセージを表示
+            st.write("正解です！")
         else:
-            st.error("不正解です。正しい答えは" + str(correct_answer) + "です。")  # エラーメッセージを表示
+            st.write("不正解です。正しい答えは", correct_answer, "です。")
 
-if __name__ == "__main__":
-    main()
+if st.button("ガチャを引き直す"):
+    del st.session_state.selected_word
+    del st.session_state.display_meaning
