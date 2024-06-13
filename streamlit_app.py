dc53d8aad1add51bd3c2b20cfb90fb87bcd374ab
AORNG <167181348+AORNG@users.noreply.github.com>
2024-06-13 09:32:39 +0000

    ｑ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import random
+import openpyxl
 
 # Montserratフォントを使ったタイトルを表示
 st.markdown("<h1 style='text-align: center; font-family: Open Sans, sans-serif;'>生物単語ガチャ</h1>", unsafe_allow_html=True)
@@ -37,29 +37,21 @@ if st.button('ガチャを引く！'):
     
     # セッションステートに選択された単語を保存
     st.session_state.selected_word = selected_word
+    st.session_state.display_meaning = False
 
 if 'selected_word' in st.session_state:
     st.title(f"Q: {st.session_state.selected_word['説明']}")
 
-    # 正解と誤答を取得
-    correct_answer = st.session_state.selected_word['単語']
-    wrong_answers = words_df[words_df['レア度'] != st.session_state.selected_word['レア度']]['単語'].tolist()
-
-    # 正解を含む選択肢をセッションステートに保存
-    options = wrong_answers + [correct_answer]
-    st.session_state.options = options
-
-    # 解答選択肢を表示
-    user_answer = st.radio("解答を選択してください", options)
+    # 解答選択肢を作成
+    answer_options = [st.session_state.selected_word['単語']] + list(words_df[words_df['レア度'] != st.session_state.selected_word['レア度']]['単語'])
+    user_answer = st.selectbox("解答を選択してください", answer_options)
 
     # 答え合わせボタン
     if st.button("答え合わせ"):
-        # 選択肢をシャッフルして、選択された項目のインデックスを取得
-        random.shuffle(options)
-        selected_index = options.index(user_answer)
+        correct_answer = st.session_state.selected_word['単語']   # 正解を定義
 
-        # 正解が最後の要素にあるかどうかを確認し、結果を表示
-        if selected_index == len(options) - 1:
+        # 解答が正しいかどうかを確認し、結果を表示
+        if user_answer.strip() == str(correct_answer):
             st.write("正解です！")
         else:
             st.write("不正解です。正しい答えは", correct_answer, "です。")
