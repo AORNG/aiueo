f1677f1fb33f72c346f7358e4eacdf0ca3ab9460
AORNG <167181348+AORNG@users.noreply.github.com>
2024-06-14 08:06:50 +0000

    q

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,10 +54,10 @@ if st.button('ガチャを引く！'):
         # 解答選択肢を表示
         user_answer = st.radio("解答を選択してください", options)
 
-        # 答え合わせボタン
-        if st.button("答え合わせ"):
-            # 解答が正しいかどうかを確認し、結果を表示
-            if user_answer.strip() == str(correct_answer):
-                st.write("正解です！")
-            else:
-                st.write("不正解です。正しい答えは", correct_answer, "です。")
+    # 答え合わせボタン
+    if st.button("答え合わせ"):
+        # 解答が正しいかどうかを確認し、結果を表示
+        if user_answer.strip() == str(correct_answer):
+            st.write("正解です！")
+        else:
+            st.write("不正解です。正しい答えは", correct_answer, "です。")
