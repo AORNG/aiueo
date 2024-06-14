c55b0849559e21051083cab9e9d5273be956c18d
AORNG <167181348+AORNG@users.noreply.github.com>
2024-06-14 09:14:18 +0000

    ｓ

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,19 +24,21 @@ def load_data():
 words_df = load_data()
 
 # ガチャ機能
-if st.button('ガチャを引く！', key='gacha_button', align='center'):
-    rarity_probs = {
-        'N': 0.4,
-        'R': 0.3,
-        'SR': 0.2,
-        'SSR': 0.1
-    }
-    chosen_rarity = np.random.choice(list(rarity_probs.keys()), p=list(rarity_probs.values()))
-    subset_df = words_df[words_df['レア度'] == chosen_rarity]
-    selected_word = subset_df.sample().iloc[0]
-    
-    # セッションステートに選択された単語を保存
-    st.session_state.selected_word = selected_word
+col1, col2, col3 = st.columns([1, 4, 1])
+with col2:
+    if st.button('ガチャを引く！'):
+        rarity_probs = {
+            'N': 0.4,
+            'R': 0.3,
+            'SR': 0.2,
+            'SSR': 0.1
+        }
+        chosen_rarity = np.random.choice(list(rarity_probs.keys()), p=list(rarity_probs.values()))
+        subset_df = words_df[words_df['レア度'] == chosen_rarity]
+        selected_word = subset_df.sample().iloc[0]
+        
+        # セッションステートに選択された単語を保存
+        st.session_state.selected_word = selected_word
 
 if 'selected_word' in st.session_state:
     st.title(f"Q: {st.session_state.selected_word['説明']}")
