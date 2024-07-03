7bf1dc8e444c9e3083317e3b492769961ccf3826
AORNG <167181348+AORNG@users.noreply.github.com>
2024-07-03 07:58:09 +0000

    wa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import random
+import openpyxl
 
 # Montserratフォントを使ったタイトルを表示
 st.markdown("<h1 style='text-align: center; font-family: Open Sans, sans-serif;'>生物単語ガチャ</h1>", unsafe_allow_html=True)
@@ -24,7 +24,7 @@ def load_data():
 words_df = load_data()
 
 # ガチャ機能
-if st.button('ガチャを引く！', key='gacha_button'):
+if st.button('ガチャを引く！'):
     rarity_probs = {
         'N': 0.4,
         'R': 0.3,
@@ -37,29 +37,20 @@ if st.button('ガチャを引く！', key='gacha_button'):
     
     # セッションステートに選択された単語を保存
     st.session_state.selected_word = selected_word
+    st.session_state.display_meaning = False
 
 if 'selected_word' in st.session_state:
     st.title(f"Q: {st.session_state.selected_word['説明']}")
+    # 解答欄を作成
+    user_answer = st.text_input("解答を入力してください")
 
-    # 正解と誤答を取得
-    correct_answer = st.session_state.selected_word['単語']
-    wrong_answers = words_df[words_df['レア度'] != st.session_state.selected_word['レア度']]['単語'].tolist()
+    # 答え合わせボタン
+    if st.button("答え合わせ"):
+        correct_answer = st.session_state.selected_word['単語']   # 正解を定義
 
-    # 正解を含む選択肢をランダムに選ぶ
-    options = random.sample(wrong_answers, 3) + [correct_answer]
+        # 解答が正しいかどうかを確認し、結果を表示
+        if user_answer.strip() == str(correct_answer):
+            st.write("正解です！")
+        else:
+            st.write("不正解です。正しい答えは", correct_answer, "です。")
 
-    # 選択肢をシャッフル
-    random.shuffle(options)
-
-    # 解答選択肢を表示
-    user_answer = st.radio("解答を選択してください", options)
-
-    # 解答が正しいかどうかを確認し、結果を表示
-    if user_answer.strip() == str(correct_answer):
-        st.write("正解です！")
-    else:
-        st.write("不正解です。正しい答えは", correct_answer, "です。")
-
-    # 正解と解答を表示
-    st.write(f"正しい答え: {correct_answer}")
-    st.write(f"あなたの答え: {user_answer}")
