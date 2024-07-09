d63546fb9ef1dc1f3aed72aff26ed732f981f959
AORNG <167181348+AORNG@users.noreply.github.com>
2024-07-09 08:23:04 +0000

    i

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,5 +79,3 @@ def main():
                 else:
                     st.error("不正解です。")
                     st.write(f"正解は {st.session_state.correct_answer}")
-if __name__ == '__main__':
-    main()
