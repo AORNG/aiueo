13abb543f2d79c57f54083f0655a2d41350542d9
AORNG <167181348+AORNG@users.noreply.github.com>
2024-07-24 07:49:43 +0000

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,7 +122,7 @@ if 'selected_word' in st.session_state:
                 feedback_container.success("正解!")
                 st.session_state.score += 10  # 正解の場合に点数を追加
             else:
-                feedback_container.error("不正解"
+                feedback_container.error("不正解",
                                          f"正解は {st.session_state.correct_answer}")
                 st.session_state.score = max(st.session_state.score - 10, 0)  # 不正解の場合に点数を減らす
 
