f50f91594ef0fbe717a19920c30251c513110f0a
AORNG <167181348+AORNG@users.noreply.github.com>
2024-07-30 01:09:17 +0000

    ｓ

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 
 # タブの選択
-tab_selection = st.sidebar.radio("Navigation", ["ガチャ", "スコア"])
+tab_selection = st.sidebar.radio("Navigation", ["第一章、第二章", "スコア"])
 
 # Montserratフォントを使ったタイトルを表示
 st.markdown("<h1 style='text-align: center; font-family: Open Sans, sans-serif;'>生物単語ガチャ</h1>", unsafe_allow_html=True)
