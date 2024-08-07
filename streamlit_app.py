cf598197d44267026b5995f396168ed6cee73e00
AORNG <167181348+AORNG@users.noreply.github.com>
2024-08-07 02:09:13 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ st.sidebar.write("※ダブルクリックしてください")
 
 # ガチャタブのコンテンツ
 if tab_selection == "第一章、第二章":
-    st.header("顕微鏡,細胞,代謝")
+    st.header("顕微鏡,生物の特徴,遺伝子とその働き")
 
     # クイズを表示するためのトグルボタン
     show_quiz = st.button("ガチャを引く！")
